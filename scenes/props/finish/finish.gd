a9890b333c17fa424f441790859675098f731cf5
JostMK <jost.klink@gmail.com>
2024-11-11 14:15:48 +0100

    Adds finish area

---
+++ scenes/props/finish/finish.gd
@@ -0,0 +1,34 @@
+@tool
+extends Area2D
+class_name Finish
+
+signal Finished
+
+@export var size: Vector2 = Vector2(100, 100):
+	set(value):
+		size = value
+		_update_visuals()
+
+
+@onready var collision_shape: CollisionShape2D = %CollisionShape2D
+@onready var sprite: Sprite2D = %Sprite2D
+
+func _ready() -> void:
+	_update_visuals()
+
+	body_entered.connect(_on_body_entered)
+
+
+func _update_visuals() -> void:
+	if not collision_shape or not sprite:
+		return
+	
+	var shape: RectangleShape2D = RectangleShape2D.new()
+	shape.size = size
+	collision_shape.shape = shape
+
+	sprite.scale = Vector2(size.x / 256, size.y / 256)
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	Finished.emit()
