301fae5e720253def34c91bb481451ac2f7ab03d
JostMK <jost.klink@gmail.com>
2024-11-17 21:57:47 +0100

    Adds level progression

--- components/game_orchestrator.gd
+++ components/game_orchestrator.gd
@@ -10,18 +10,23 @@ enum LevelState {
 @export var level_ui: LevelUI
 @export var follow_camera: FollowCamera
 
-@export var temp_scene_ref: PackedScene
+@export var levels: Array[PackedScene]
+
+var _current_level_index: int
 
 var _current_level: Level
 var _current_level_scene: PackedScene
 var _state: LevelState
 
 func _ready() -> void:
-	# for testing
-	call_deferred("load_level", temp_scene_ref)
+
+	_current_level_index = 0
 
 	level_ui.StartLevel.connect(start_level)
+	level_ui.NextLevel.connect(next_level)
 	level_ui.call_deferred("toggle_stats_text", false)
+
+	call_deferred("load_level", levels[_current_level_index])
 	
 
 func load_level(level: PackedScene) -> void:
@@ -39,10 +44,15 @@ func load_level(level: PackedScene) -> void:
 	_state = LevelState.START
 	level_ui.set_status_text("Godot Rush")
 	level_ui.set_button_text("Start Game")
+
+	level_ui.show_level_retry()
 	level_ui.show()
 
 
 func start_level() -> void:
+	if not _current_level:
+		return
+
 	level_ui.hide()
 
 	match _state:
@@ -54,7 +64,19 @@ func start_level() -> void:
 
 	# activates processing for level only in the next frame to avoid conflicts
 	_call_next_frame(_current_level.start)
-	
+
+
+func next_level() -> void:
+	_current_level_index  = (_current_level_index + 1) % levels.size()
+
+	_current_level.name = "DELETED"
+	_current_level.queue_free()
+	_current_level = null
+
+	level_ui.toggle_stats_text(false)
+
+	call_deferred("load_level", levels[_current_level_index])
+
 
 func prepare_retry_level() -> void:
 	follow_camera.set_target(null)
@@ -89,6 +111,7 @@ func _on_player_finished() -> void:
 	level_ui.set_stats_text(_current_level.get_reflect_stat())
 	level_ui.toggle_stats_text(true)
 
+	level_ui.show_level_finished()
 	level_ui.show()
 
 
@@ -96,6 +119,8 @@ func _on_player_died() -> void:
 	_state = LevelState.RETRY
 	level_ui.set_status_text("Godot Died!")
 	level_ui.set_button_text("Try Again")
+
+	level_ui.show_level_retry()
 	level_ui.show()
 
 
--- scenes/ui/level/level_ui.gd
+++ scenes/ui/level/level_ui.gd
@@ -2,13 +2,19 @@ extends Control
 class_name LevelUI
 
 signal StartLevel
+signal NextLevel
 
 @onready var status_label: Label = %StatusLabel
 @onready var start_button: Button = %StartButton
 @onready var stats_label: Label = %StatsLabel
+@onready var level_finished_menu: Control = %LevelFinishedMenu
+@onready var replay_button: Button = %ReplayButton
+@onready var next_button: Button = %NextButton
 
 func _ready() -> void:
 	start_button.pressed.connect(StartLevel.emit)
+	replay_button.pressed.connect(StartLevel.emit)
+	next_button.pressed.connect(NextLevel.emit)
 
 
 func set_status_text(text: String) -> void:
@@ -28,3 +34,13 @@ func toggle_stats_text(enable: bool) -> void:
 
 func set_stats_text(reflect_count: int) -> void:
 	stats_label.text = "Reflections used: %d" % reflect_count
+
+
+func show_level_finished() -> void:
+	start_button.hide()
+	level_finished_menu.show()
+
+
+func show_level_retry() -> void:
+	start_button.show()
+	level_finished_menu.hide()
