0cff492fec3193829725c78db09937e7546bd31c
JostMK <jost.klink@gmail.com>
2024-11-30 23:00:21 +0100

    Ensures visibility of physic shape debug visuals
    Sets the z index of the damage area to ensure the debug visuals are always drawn on top

--- components/damage_area.gd
+++ components/damage_area.gd
@@ -1,16 +1,23 @@
+@tool
 extends Area2D
 class_name DamageArea
 
 func _ready() -> void:
-    body_entered.connect(_on_body_entered)
+	# to make sure collision shapes always get drawn on top when debugging
+	self.z_index = 1000
 
+	if Engine.is_editor_hint():
+		return
 
+	body_entered.connect(_on_body_entered)
+
+	
 func _on_body_entered(body: Node2D) -> void:
-    var player = body as Player
-    if not player:
-        return
+	var player = body as Player
+	if not player:
+		return
 
-    if player.is_dead():
-        return
+	if player.is_dead():
+		return
 
-    player.damage()
+	player.damage()
